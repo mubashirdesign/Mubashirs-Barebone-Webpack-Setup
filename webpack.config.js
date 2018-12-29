7d3baebf24dd127010139b2eccc848270165a022
Mubashir Cheentavida <msbmubashir@gmail.com>
2018-12-29 18:50:16 -0500

    updated webpack and package.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,21 @@
 const path = require('path'); //absolute path for output
+//HTML plugin to export index.html and minify HTML
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+//Copies the images from SRC into distribution on build
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+//Extracts and minifies the CSS/SASS files
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+//Minifies the images
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
+
+const imageminMozjpeg = require('imagemin-mozjpeg');
+
+//Auto prefixes CSS files for compatability (i.e -webkit)
+const autoprefixer = require('autoprefixer');
+
 
 module.exports = {
 //ENTRY POINT, where webpack starts bundling i.e dependancies
@@ -13,29 +27,43 @@ module.exports = {
  	filename: 'js/bundle.js'
  },
  
+ //Dev server pulls from here
  devServer: {
  	contentBase: './dist'
  },
 
  plugins: [
 
- new ExtractTextPlugin('css/styles.css'),
 
- new HtmlWebpackPlugin({
+	new MiniCssExtractPlugin({
+		filename: "css/styles.css",
+		chunkFilename: "css/[id].css"
+	  }),
 
- 	filename: 'index.html',
- 	template: './src/index.html'
+	new HtmlWebpackPlugin({
+		filename: 'index.html',
+		template: './src/index.html',
+		minify: {
+			collapseWhitespace: true,
+			removeComments: true,
+			removeRedundantAttributes: true,
+			removeScriptTypeAttributes: true,
+			removeStyleLinkTypeAttributes: true,
+			useShortDoctype: true
+		}
+	}),
 
- }),
 
+ 	new CopyWebpackPlugin([
+        {from:'src/img',to:'img'} 
+	]),
 
- new CopyWebpackPlugin([
-            {from:'src/img',to:'img'} 
-        ])
+	new ImageminPlugin({ 
+		pngquant: ({quality: '80'}),
+		plugins: [imageminMozjpeg({quality: '80'})]
+	}),
 
-
- ],
-
+	],
  	module: {
 
  		rules: [
@@ -50,11 +78,35 @@ module.exports = {
 
  		{
                 test:/\.(s*)css$/, 
-                use: ExtractTextPlugin.extract({ 
-                        fallback:'style-loader',
-                        use:['css-loader','sass-loader'],
-                    })
-        }
+                use: [
+					MiniCssExtractPlugin.loader,
+					//Loads any CSS file
+					{
+						loader: "css-loader"
+					},
+						//Post the CSS load, apply auto prefixing and minify CSS
+						{
+							loader: "postcss-loader",
+							options: {
+								autoprefixer: {
+									browsers: ["last 10 versions"]
+								},
+								plugins: () => [
+									//Auto prefixes with -webkit on CSS
+									autoprefixer,
+									//Minifies the CSS
+									require('cssnano')()
+								]
+							},
+						},
+					//Loads any Sass/SCSS filess
+					{
+						loader: "sass-loader",
+						options: {}
+					}
+					
+				],		
+		}
 
  	]
 }
